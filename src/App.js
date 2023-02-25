c24cae458571ac731370ed8bc7c3912603dec531
Frank Barrett <frankebarrett@gmail.com>
2023-02-24 17:26:32 -0700

    Re-created original site in React

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import profileImage from "./profile.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={profileImage} className="profile-image" alt="profile-image" />
+        <h1>Frank Barrett</h1>
+        <p className="links">
+          <a
+            className="App-link"
+            href="https://github.com/fjbarrett"
+            target="_blank"
+          >
+            Github
+          </a>
+          <a
+            className="App-link"
+            href="https://twitter.com/fjbarrett"
+            target="_blank"
+          >
+            Twitter
+          </a>
+          <a
+            className="App-link"
+            href="https://instagram.com/fjbarrett"
+            target="_blank"
+          >
+            Instagram
+          </a>
+          <a
+            className="App-link"
+            href="https://facebook.com/fjbarrett"
+            target="_blank"
+          >
+            Facebook
+          </a>
+          <a
+            className="App-link"
+            href="https://twitch.com/fjbarrett"
+            target="_blank"
+          >
+            Twitch
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
